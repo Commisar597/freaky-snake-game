f263da1e65a30a4582f1982fa5031f88382c0858
Sofia061 <kotbax06@gmail.com>
2025-11-17 22:49:48 +0100

    fruit random pop up - don "FKN TIRED"

--- js/js.js
+++ js/js.js
@@ -1,43 +1,99 @@
 //initialize an array with colors for setting the background color
+var bgColor = ["#ffe4eaff", "#0C1226", "#dfffe7ff"];
+
 const grid = document.querySelector(".screen");
 const playbutton = document.getElementById("playbutton");
 var player_positions = [120, 136, 152, 168, 184];
 var direction = "up";
-//loading the variable with the color in the local storage
+const fruit = document.getElementById("fruitsId");
 
-var snakeColors = ["rgba(160, 41, 41, 1)", "rgba(215, 254, 59, 1)", "rgba(60, 90, 210, 1)"]
-var snakeBorderColors = ["rgba(114, 30, 30, 1)", "rgba(140, 165, 40, 1)", "rgba(42, 63, 146, 1)"]
-var currentSnakeColor = "rgba(160, 41, 41, 1)"
-var currentSnakeBorderColor = "rgba(114, 30, 30, 1)"
+//loading the variable with the color in the local storage
+var savedColor = localStorage.getItem("bgColor");
+if (savedColor) document.body.style.backgroundColor = savedColor;
+
+var snakeColors = [
+  "rgba(160, 41, 41, 1)",
+  "rgba(215, 254, 59, 1)",
+  "rgba(60, 90, 210, 1)",
+];
+var snakeBorderColors = [
+  "rgba(114, 30, 30, 1)",
+  "rgba(140, 165, 40, 1)",
+  "rgba(42, 63, 146, 1)",
+];
+var currentSnakeColor = "rgba(160, 41, 41, 1)";
+var currentSnakeBorderColor = "rgba(114, 30, 30, 1)";
 
 document.getElementById("pinkBG").addEventListener("click", function () {
-  document.documentElement.style.setProperty('--bg-layout-color', '#ffe4eaff');
-  document.documentElement.style.setProperty('--bg-block-border-color', '#ffdde5');
-  document.documentElement.style.setProperty('--bg-block-color', '#ffbfcc');
-  document.documentElement.style.setProperty('--text-primary-color', '#582B41');
-  document.documentElement.style.setProperty('--text-secondary-color', '#7F4B61');
-  document.documentElement.style.setProperty('--text-highlighted-color', '#E3006F');
-  document.documentElement.style.setProperty('--text-highlighted2-color', '#FF78A7');
+  document.body.style.backgroundColor = bgColor[0];
+  localStorage.setItem("bgColor", "#ffe4eaff"); //saving the bg color in local storage
+  document.documentElement.style.setProperty(
+    "--bg-block-border-color",
+    "#ffdde5"
+  );
+  document.documentElement.style.setProperty("--bg-block-color", "#ffbfcc");
+  document.documentElement.style.setProperty("--text-primary-color", "#582B41");
+  document.documentElement.style.setProperty(
+    "--text-secondary-color",
+    "#7F4B61"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted-color",
+    "#E3006F"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted2-color",
+    "#FF78A7"
+  );
 });
 
 document.getElementById("blueBG").addEventListener("click", function () {
-  document.documentElement.style.setProperty('--bg-layout-color', '#0C1226');
-  document.documentElement.style.setProperty('--bg-block-border-color', '#1B274A');
-  document.documentElement.style.setProperty('--bg-block-color', '#1F315C');
-  document.documentElement.style.setProperty('--text-primary-color', '#202430ff');
-  document.documentElement.style.setProperty('--text-secondary-color', '#8FA2D0');
-  document.documentElement.style.setProperty('--text-highlighted-color', '#5A8BFF');
-  document.documentElement.style.setProperty('--text-highlighted2-color', '#89A8FF');
+  document.body.style.backgroundColor = bgColor[1];
+  localStorage.setItem("bgColor", "#0C1226");
+  document.documentElement.style.setProperty(
+    "--bg-block-border-color",
+    "#1B274A"
+  );
+  document.documentElement.style.setProperty("--bg-block-color", "#1F315C");
+  document.documentElement.style.setProperty(
+    "--text-primary-color",
+    "#202430ff"
+  );
+  document.documentElement.style.setProperty(
+    "--text-secondary-color",
+    "#8FA2D0"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted-color",
+    "#5A8BFF"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted2-color",
+    "#89A8FF"
+  );
 });
 
 document.getElementById("greenBG").addEventListener("click", function () {
-  document.documentElement.style.setProperty('--bg-layout-color', '#dfffe7ff');
-  document.documentElement.style.setProperty('--bg-block-border-color', '#C7EAD1');
-  document.documentElement.style.setProperty('--bg-block-color', '#AEECC0');
-  document.documentElement.style.setProperty('--text-primary-color', '#1F4B2E');
-  document.documentElement.style.setProperty('--text-secondary-color', '#4E7257');
-  document.documentElement.style.setProperty('--text-highlighted-color', '#27A05A');
-  document.documentElement.style.setProperty('--text-highlighted2-color', '#57D98B');
+  document.body.style.backgroundColor = bgColor[2];
+  localStorage.setItem("bgColor", "#dfffe7ff");
+  document.documentElement.style.setProperty(
+    "--bg-block-border-color",
+    "#C7EAD1"
+  );
+  document.documentElement.style.setProperty("--bg-block-color", "#AEECC0");
+  document.documentElement.style.setProperty("--text-primary-color", "#1F4B2E");
+  document.documentElement.style.setProperty(
+    "--text-secondary-color",
+    "#4E7257"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted-color",
+    "#27A05A"
+  );
+  document.documentElement.style.setProperty(
+    "--text-highlighted2-color",
+    "#57D98B"
+  );
 });
 
 document
@@ -65,44 +121,93 @@ let speed = 500;
 let speedIncrease = 10;
 let minSpeed = 80;
 
+//hiding the fruit object
+if (fruit) {
+  fruit.style.display = "none";
+}
+
 function CreateGrid() {
   let time = 0;
 
-  //I put the timer inside the button cuz it just keeps going up if its not, also would be preferable to stop the timer somehow H.N.  
+  //I put the timer inside the button cuz it just keeps going up if its not, also would be preferable to stop the timer somehow H.N.
   setInterval(() => {
     time++;
-    timerEvents.dispatchEvent(new CustomEvent("tick", {
-      detail: { value: 1 }
-    }));
-  }, 120);//Higher value = slower snake, I think this speed is good H.N.
+    timerEvents.dispatchEvent(
+      new CustomEvent("tick", {
+        detail: { value: 1 },
+      })
+    );
+  }, 120); //Higher value = slower snake, I think this speed is good H.N.
 
   grid.innerHTML = ""; // this just clears existing cells, may be redunant
   cells = [];
-  for (let i = 0; i < 256; i++) {//Creates 256 divs for the grid, configure size in CSS .screen 
+  grid.appendChild(fruit); //showing the fruit object
+  for (let i = 0; i < 256; i++) {
+    //Creates 256 divs for the grid, configure size in CSS .screen
     const cell = document.createElement("div");
     cell.className = "cell";
     grid.appendChild(cell); // put the cells as the childs of the grid
-    cells.push(cell);// and in the array as elements
+    cells.push(cell); // and in the array as elements
   }
-  player_positions.forEach(pos => {
+  player_positions.forEach((pos) => {
     cells[pos].style.backgroundColor = currentSnakeColor;
     cells[pos].style.border = "2px solid " + currentSnakeBorderColor;
   });
   timerEvents.addEventListener("tick", (event) => {
     movePlayer(direction);
-  })
+  });
+
+  moveFruit();
 }
 
-const timerEvents = new EventTarget;
+//random fruits
+function moveFruit() {
+  const availableCells = []; //an array for available cells
+  //loop to found the cells that are not part of the snake
+  for (let i = 0; i < cells.length; i++) {
+    if (!player_positions.includes(i)) {
+      availableCells.push(i);
+    }
+  }
+
+  if (availableCells.length > 0) {
+    //picks a random position from available cells
+    const randomIndex = Math.floor(Math.random() * availableCells.length);
+    fruitPosition = availableCells[randomIndex]; //appends it
+
+    //checks if the fruit element was created
+    if (fruit) {
+      cells[fruitPosition].appendChild(fruit); //moves element to the chosen cell
+      fruit.style.display = "block"; //make the fruit object visible
+    }
+  }
+}
+
+const timerEvents = new EventTarget();
 playbutton.addEventListener("click", CreateGrid);
 
 function movePlayer(direction) {
   let headPosition = player_positions[0];
   let newHeadPosition;
-  if (direction === "up") { newHeadPosition = headPosition - 16; };
-  if (direction === "down") { newHeadPosition = headPosition + 16; };
-  if (direction === "left") { newHeadPosition = headPosition - 1; };
-  if (direction === "right") { newHeadPosition = headPosition + 1; };
+  if (direction === "up") {
+    newHeadPosition = headPosition - 16;
+  }
+  if (direction === "down") {
+    newHeadPosition = headPosition + 16;
+  }
+  if (direction === "left") {
+    newHeadPosition = headPosition - 1;
+  }
+  if (direction === "right") {
+    newHeadPosition = headPosition + 1;
+  }
+
+  const ateFruit = newHeadPosition === fruitPosition; //checks the equality of the snake head and the fruit cell
+
+  //moves the fruit if the equality is true
+  if (ateFruit) {
+    moveFruit();
+  }
 
   let tail = player_positions.pop();
   cells[tail].style.backgroundColor = "rgb(102, 145, 74)";
@@ -127,7 +232,6 @@ document.addEventListener("keydown", (event) => {
     direction = "right";
   }
 });
-
 
 function autoMoveLoop() {
   autoMoveSnake();

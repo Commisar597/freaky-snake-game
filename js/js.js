fe9a59d32163d66c1fbb897ab110cb1347ebe2ae
Artem Salatskyi <78max02@gmail.com>
2025-11-17 18:38:32 +0200

    finish of d.part

--- js/js.js
+++ js/js.js
@@ -1,23 +1,18 @@
 //initialize an array with colors for setting the background color
-var bgColor = ["#FFF6F8", "#0C1226", "#F3FFF6"];
 const grid = document.querySelector(".screen");
 const playbutton = document.getElementById("playbutton");
 var player_positions = [120, 136, 152, 168, 184];
 var direction = "up";
 //loading the variable with the color in the local storage
-var savedColor = localStorage.getItem("bgColor");
-if (savedColor) {
-  document.body.style.backgroundColor = savedColor;
-}
 
 var snakeColors = ["rgba(160, 41, 41, 1)", "rgba(215, 254, 59, 1)", "rgba(60, 90, 210, 1)"]
-var snakeBorderColors = ["rgba(114, 30, 30, 1)", "rgba(140, 165, 40, 1)", "rgba(60, 90, 210, 1)"]
+var snakeBorderColors = ["rgba(114, 30, 30, 1)", "rgba(140, 165, 40, 1)", "rgba(42, 63, 146, 1)"]
 var currentSnakeColor = "rgba(160, 41, 41, 1)"
 var currentSnakeBorderColor = "rgba(114, 30, 30, 1)"
- 
+
 // Палетка A — Розовая |
 document.getElementById("pinkBG").addEventListener("click", function () {
-  document.documentElement.style.setProperty('--bg-layout-color', '#FFF6F8');
+  document.documentElement.style.setProperty('--bg-layout-color', '#ffe4eaff');
   document.documentElement.style.setProperty('--bg-block-border-color', '#ffdde5');
   document.documentElement.style.setProperty('--bg-block-color', '#ffbfcc');
   document.documentElement.style.setProperty('--text-primary-color', '#582B41');
@@ -31,7 +26,7 @@ document.getElementById("blueBG").addEventListener("click", function () {
   document.documentElement.style.setProperty('--bg-layout-color', '#0C1226');
   document.documentElement.style.setProperty('--bg-block-border-color', '#1B274A');
   document.documentElement.style.setProperty('--bg-block-color', '#1F315C');
-  document.documentElement.style.setProperty('--text-primary-color', '#C9D6FF');
+  document.documentElement.style.setProperty('--text-primary-color', '#202430ff');
   document.documentElement.style.setProperty('--text-secondary-color', '#8FA2D0');
   document.documentElement.style.setProperty('--text-highlighted-color', '#5A8BFF');
   document.documentElement.style.setProperty('--text-highlighted2-color', '#89A8FF');
@@ -39,7 +34,7 @@ document.getElementById("blueBG").addEventListener("click", function () {
 
 // Палетка C — Приятная зелёная |
 document.getElementById("greenBG").addEventListener("click", function () {
-  document.documentElement.style.setProperty('--bg-layout-color', '#F3FFF6');
+  document.documentElement.style.setProperty('--bg-layout-color', '#dfffe7ff');
   document.documentElement.style.setProperty('--bg-block-border-color', '#C7EAD1');
   document.documentElement.style.setProperty('--bg-block-color', '#AEECC0');
   document.documentElement.style.setProperty('--text-primary-color', '#1F4B2E');
@@ -48,21 +43,25 @@ document.getElementById("greenBG").addEventListener("click", function () {
   document.documentElement.style.setProperty('--text-highlighted2-color', '#57D98B');
 });
 
-document.getElementById("redBackground") // snake colour
+document
+  .getElementById("redColorSnake") // snake colour
   .addEventListener("click", function () {
-
+    currentSnakeBorderColor = snakeBorderColors[0];
+    currentSnakeColor = snakeColors[0];
   });
 
 document
   .getElementById("yellowColorSnake")
   .addEventListener("click", function () {
-
+    currentSnakeBorderColor = snakeBorderColors[1];
+    currentSnakeColor = snakeColors[1];
   });
 
 document
   .getElementById("blueColorSnake")
   .addEventListener("click", function () {
-
+    currentSnakeBorderColor = snakeBorderColors[2];
+    currentSnakeColor = snakeColors[2];
   });
 
 let speed = 500;
@@ -100,20 +99,22 @@ function CreateGrid() {
 const timerEvents = new EventTarget;
 playbutton.addEventListener("click", CreateGrid);
 
-  function movePlayer(direction){
+function movePlayer(direction) {
   let headPosition = player_positions[0];
   let newHeadPosition;
-  if (direction === "up"){newHeadPosition = headPosition - 16;};
-  if (direction === "down"){newHeadPosition = headPosition + 16;};
-  if (direction === "left"){newHeadPosition = headPosition - 1;};
-  if (direction === "right"){newHeadPosition = headPosition + 1;};
+  if (direction === "up") { newHeadPosition = headPosition - 16; };
+  if (direction === "down") { newHeadPosition = headPosition + 16; };
+  if (direction === "left") { newHeadPosition = headPosition - 1; };
+  if (direction === "right") { newHeadPosition = headPosition + 1; };
 
-  let tail = player_positions.pop(); // .pop removes the last element from the array, and returns it, so tail var is the removed element
-  cells[tail].style.backgroundColor = "white"; // sets the tail white
-  player_positions.unshift(newHeadPosition);// .unshift adds an element to the start of the array, unlike push which adds to the end, cool ik
-  cells[newHeadPosition].style.backgroundColor = "black";
+  let tail = player_positions.pop();
+  cells[tail].style.backgroundColor = "rgb(102, 145, 74)";
+  cells[tail].style.border = "2px solid rgb(64, 98, 65)";
+  player_positions.unshift(newHeadPosition);
+  cells[newHeadPosition].style.backgroundColor = currentSnakeColor;
+  cells[newHeadPosition].style.border = "2px solid " + currentSnakeBorderColor;
 }
-  document.addEventListener("keydown", (event) => {
+document.addEventListener("keydown", (event) => {
   if (cells.length === 0) return;
 
   if (event.key === "w" || event.key === "W" || event.key === "ArrowUp") {
@@ -128,15 +129,14 @@ playbutton.addEventListener("click", CreateGrid);
   if (event.key === "d" || event.key === "D" || event.key === "ArrowRight") {
     direction = "right";
   }
+});
 
-function autoMoveLoop()
-{
+
+function autoMoveLoop() {
   autoMoveSnake();
 
   speed -= speedIncrease;
-  if(speed < minSpeed) speed = minSpeed;
+  if (speed < minSpeed) speed = minSpeed;
 
   setTimeout(autoMoveLoop, speed);
 }
-
-});
